102cb7483734a2f7659753cff2918be5843b90cc
Vikash Harjeewan <vikash@backgroundcheck.email>
2018-02-09 15:21:57 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,27 +1,27 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
-
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
-
-// You don't have to do things with the ScraperWiki library.
-// You can use whatever libraries you want: https://morph.io/documentation/php
-// All that matters is that your final data is written to an SQLite database
-// called "data.sqlite" in the current working directory which has at least a table
-// called "data".
+require 'scraperwiki.php';
+require 'scraperwiki/simple_html_dom.php';
+$years	= array('2000');
+for ($mainpage = 0; $mainpage < sizeof($years); $mainpage++)
+{
+	$x = 1; 
+	$linkabc	=	'http://supremecourtofindia.nic.in/php/case_status/case_status_process.php?d_no='.$x.'&d_yr='.$years[$mainpage];
+	$htmlcheck		=	file_get_html($linkabc);
+	$checking		=	$htmlcheck->find("h5[plaintext^=Diary No]",0)->plaintext;
+	while($checking != "")
+	{
+		$link	=	'http://supremecourtofindia.nic.in/php/case_status/case_status_process.php?d_no='.$x.'&d_yr='.$years[$mainpage];
+		$html	=	file_get_html($link);
+		if($html)
+		{
+		$check	=	$html->find("h5[plaintext^=Diary No]",0)->plaintext;
+		$record = array('no' => $x ,'link' => $link ,  'check' =>$check);
+		scraperwiki::save(array('no','link','check'), $record);
+		echo "$check\n";
+		$x++;
+		}
+		
+		
+	}
+}
 ?>
